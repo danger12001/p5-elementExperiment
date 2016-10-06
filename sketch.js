34612e704dd70335d9e282e8c263a5f639a29b27
danger12001 <paul@projectcodex.co>
2016-10-06 22:18:48 +0200

    hydrogen added

--- sketch.js
+++ sketch.js
@@ -1,6 +1,16 @@
-function setup(){
 
+
+function setup(){
+createCanvas(windowWidth,windowHeight);
 }
 function draw(){
-	
+background(0);
 }
+
+	function hydrogen(){
+		this.phase = 'gas';
+		this.weight = 1.008;
+		this.symbol = 'H';
+		this.flammable = 1;
+		this.radius = 25;
+	}
